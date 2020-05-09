cbf8b896bd927fbfc9b1a7b9ff8e9a0cd6b63f43
Mrzhouhua <1125394713@qq.com>
2020-05-10 00:06:12 +0800

    spring-mybatis

---
+++ src/main/java/com/student/community/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.student.community.mapper;
+
+import com.student.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_Id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
